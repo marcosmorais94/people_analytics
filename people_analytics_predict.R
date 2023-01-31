d31f4ff504bbb69a16e7e94c9ca17b616c5012f9
Marcos de Morais Silva <91103250+marcosmorais94@users.noreply.github.com>
2023-01-30 21:00:09 -0300

    Add files via upload

--- people_analytics_predict.R
+++ people_analytics_predict.R
@@ -71,3 +71,82 @@ dados <- droplevels(bd_rh)
 str(bd_rh)
 summary(bd_rh)
 View(bd_rh)
+
+#### 5 - Engenharia de Atributos ####
+# Nesta etapa vamos incluir alguns atributos que não foram identificadas na base original
+# Contudo, são informações que podemos incluir a partir do dataset original
+
+# Pior Year of Experience siginifica quantos anos o profissional tem de experiência profissional
+bd_rh$PriorYearsOfExperience <- bd_rh$TotalWorkingYears - bd_rh$YearsAtCompany
+View(bd_rh)
+
+#Average Tenure é a estabilidade média do profissional no mesmo emprego
+bd_rh$AverageTenure <- bd_rh$PriorYearsOfExperience / bd_rh$NumCompaniesWorked
+View(bd_rh)
+
+# A Average Tenure produz valores como Inf devido à natureza de sua derivação
+# É possível identificar esse valores pelo summary, neste caso na média
+summary(bd_rh$AverageTenure) 
+
+# Substituímos para zero, onde tudo que for contrário de finito será igualado a 0.
+bd_rh$AverageTenure[!is.finite(bd_rh$AverageTenure)] <- 0
+
+# Confere se ainda há valores Inf
+summary(bd_rh$AverageTenure)
+View(bd_rh)
+
+#### 6 - Análise Exploratória ####
+
+# Subset dos dados como base na coluna Termination, que indica se o funcionário foi desligado da empresa.
+bd_rh_1 <- bd_rh[bd_rh$Attrition != 'Termination',]
+bd_rh_1 <- droplevels(bd_rh_1)
+dim(bd_rh_1)
+summary(bd_rh_1)
+
+# Mesmo filtro anterior, mas agora por demissão voluntária
+bd_rh_2 <- bd_rh[bd_rh$Attrition != 'Voluntary Resignation',]
+bd_rh_2 <- droplevels(bd_rh_2)
+dim(bd_rh_2)  
+summary(bd_rh_2)
+
+# Ambos os subsets serão usado posteriormente na análise exploratória dos dados 
+# e com isso relevar insights importantes.
+
+# Plots de análise univariada
+
+# Contagem por genêro
+# Aqui vemos que a base de dados temos mais homens que mulheres na base
+ggplot(bd_rh) + geom_bar(aes(x = Gender))
+
+# Idade dos profissionais da IBM
+# A idade é em torno de 30 a 35 anos em suas grande maioria, 
+# indica que temos um boa parte dos profissionais com uma idade média
+ggplot(bd_rh) + geom_density(aes(x = Age))
+
+# Situação atual dos profissionais da base de dados
+# A grande maioria continua empregado, uma boa parcela escolheu a demissão voluntária
+# Essa fatia da demissão voluntária pode ter respostas interessantes do motivo da saída dos profissinais
+# A menor parcela são os demitidos, que pode ter insights interessantes também relacionados aos motivos da demissão em si
+ggplot(bd_rh) + geom_bar(aes(x = Attrition))
+
+# Contagem por Departamento
+# Neste gráfico vemos que a maioria dos profissionais pertecem a área de pesquisa e desenvolvimento
+# O que indica que temos na análise uma área que tende a ser muito estressante dentro da empresa
+# Historicamente a área de Pesquisa e Desenvolvimento é muito cobrada por resultados
+ggplot(bd_rh) + geom_bar(aes(x = Department))
+
+# Contagem por Cargo
+# A maioria dos profissionais é executivo de vendas seguido de pesquisadores
+# Geralmente são cargos que possuem muita cobranças e prazos curtos de entrega, o que implica em stress
+ggplot(bd_rh) + geom_bar(aes(x = JobRole))
+
+
+ggplot(bd_rh) + geom_bar(aes(x = Education)) + facet_grid(~EducationField)
+
+# Multiplot Grid
+p.TotalWorkingYears       <- ggplot(bd_rh) + geom_density(aes(TotalWorkingYears))
+p.YearsAtCompany          <- ggplot(bd_rh) + geom_density(aes(YearsAtCompany))
+p.YearsSinceLastPromotion <- ggplot(bd_rh) + geom_density(aes(YearsSinceLastPromotion))
+p.YearsWithCurrManager    <- ggplot(bd_rh) + geom_density(aes(YearsWithCurrManager))
+p.YearsInCurrentRole      <- ggplot(bd_rh) + geom_density(aes(YearsInCurrentRole))
+p.PriorYearsOfExperience  <- ggplot(bd_rh) + geom_density(aes(PriorYearsOfExperience))
